4af1d9cc262c73f7cb5c066fe76972917b2e2a10
John Cedrick <johnlibradilla38@gmail.com>
2026-07-09 00:40:28 +0800

    Convert _app to JS to fix Vercel CSS import type error permanently

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import { Inter } from 'next/font/google';
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <main className={`${inter.variable} font-sans min-h-screen bg-dark-900 text-slate-100 flex flex-col`}>
       <Component {...pageProps} />
